61f1993fc3c7b9017ebb5868b6d3ff976474be1b
Michel Novus <michelnovus@gmail.com>
2023-12-12 19:26:34 -0300

    Refactorizado módulo ipc.py, solo falta el manejo apropiado de las excepciones.

--- localdoc/ipc.py
+++ localdoc/ipc.py
@@ -1,117 +1,92 @@
 # [GLP3] Copyright (C) 2024  Michel Novus
 
 import os
-import os.path
 import socket
 import json
-from enum import Enum
-
-IPCType = Enum("ICPType", ["CLIENT", "SERVER"])
+from typing import Any
 
 
-class IPC(object):
-    """Permite recibir y enviar datos entre dos procesos independientes
-    mediante sockets UNIX.
+def serialize(data: dict[str, Any]) -> bytes:
+    """Serializa un 'dict' a un formato JSON binario."""
+    if type(data) != type(dict()):
+        raise TypeError("se esperaba un elemento tipo dict()")
+    json_data = json.dumps(data, separators=(",", ":"))
+    return json_data.encode("utf-8")
 
-    El IPC tipo CLIENT implementa el método communicate(), envía un dict
-    al IPC tipo SERVER y espera su respuesta, otro dict.
 
-    El IPC tipo SERVER implementa los métodos wait() y reply(). Utiliza el
-    método wait() para esperar conexiones de otros procesos y utiliza después
-    reply() para responder y terminar la conexion.
-    """
+def deserialize(data: bytes) -> dict[str, Any]:
+    """Deserializa una cadena JSON binaria a un 'dict'."""
+    json_data = json.loads(data)
+    if type(json_data) != type(dict()):
+        raise TypeError("se esperaba un elemento tipo dict()")
+    return json_data
 
+
+def _mksocket(timeout: float = 1.0) -> socket.socket:
+    """Crea un socket AF_UNIX."""
+    unix_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    unix_socket.settimeout(timeout)
+    return unix_socket
+
+
+def communicate(
+    ipc_socket: str,
+    data: dict[str, Any],
+    timeout: float = 1.0,
+    max_buffer_size: int = 65536,
+) -> dict[str, Any]:
+    """Envía el contenido de 'data' al 'ipc_socket' y espera su respuesta."""
+
+    serialized_data = serialize(data)
+    if len(serialized_data) > max_buffer_size:
+        raise OverflowError(
+            f"el tamaño del mensaje es superior a {max_buffer_size} bytes"
+        )
+    unix_socket = _mksocket(timeout)
+    unix_socket.connect(ipc_socket)
+    amount_sent = unix_socket.send(serialized_data)
+
+    response = unix_socket.recv(max_buffer_size)
+    deserialize_response = deserialize(response)
+    return deserialize_response
+
+
+class IpcServer(object):
     def __init__(
-        self, socket_path: str, type: IPCType, timeout: float = 1.0
+        self,
+        ipc_socket: str,
+        timeout: float = 1.0,
+        max_buffer_size: int = 65536,
     ) -> None:
-        self.type = type
-        self.socket_path = socket_path
-        self.timeout = timeout
-        self.buffer_size = 65536  # 64KiB
+        self.max_buffer_size = max_buffer_size
+        self.ipc_socket = ipc_socket
+        self._socket = _mksocket(timeout)
+        self._socket.bind(self.ipc_socket)
 
-        self._connection: socket.socket = None  # type: ignore
+        self._connection = None
 
-        if self.type == IPCType.SERVER:
-            if os.path.exists(self.socket_path):
-                raise FileExistsError(f"Ya existe el socket {self.socket_path}")
-            self.unix_socket = self._mksocket()
-            self.unix_socket.bind(self.socket_path)
-            self.unix_socket.listen(1)
-        elif self.type == IPCType.CLIENT:
-            if not os.path.exists(self.socket_path):
-                raise FileNotFoundError("No existe el socket")
+    def wait_connection(self) -> dict[str, Any]:
+        """Espera por conexiones de clientes."""
 
-        if not (self.type == IPCType.CLIENT or self.type == IPCType.SERVER):
-            raise ValueError(f"type: {self.type} no es válido!")
-
-    def communicate(self, data: dict[str, str]) -> dict[str, str]:
-        """Envía un 'dict' al otro proceso y espera su respuesta como 'dict'.
-        Solo disponible en IPC typo cliente."""
-        self._raise_type_error(target_type=IPCType.CLIENT)
-        serialized_data = self._serialize(data)
-        if len(serialized_data) > self.buffer_size:
-            raise OverflowError(
-                f"se esperaba 'data' <= {self.buffer_size} bytes, se tiene "
-                f"'data' == {len(serialized_data)} bytes."
-            )
-        unix_socket = self._mksocket()
-        unix_socket.connect(self.socket_path)
-        unix_socket.sendall(serialized_data)
-        response = self._deserialize(unix_socket.recv(self.buffer_size))
-        return response
-
-    def wait(self) -> dict[str, str]:
-        """Espera una solicitud de un proceso ajeno. Llamada bloqueante.
-
-        Solo disponible en IPC typo servidor.
-
-        Debe estar seguido del método reply().
-        """
-        self._raise_type_error(target_type=IPCType.SERVER)
-        self._connection = self.unix_socket.accept()[0]
-        data = self._deserialize(self._connection.recv(self.buffer_size))
+        self._conn = self._socket.accept()[0]
+        data = self._conn.recv(self.max_buffer_size)
+        data = deserialize(data)
         return data
 
-    def reply(self, data: dict[str, str]) -> None:
-        """Mensaje de respuesta del servidor al cliente, se debe usar después
-        del método wait()."""
-        self._raise_type_error(target_type=IPCType.SERVER)
-        serialized_data = self._serialize(data)
-        if len(serialized_data) > self.buffer_size:
+    def reply(self, data: dict[str, Any]) -> None:
+        """Responde a la comunicación en curso y cierra el socket."""
+        serialized_data = serialize(data)
+        if len(serialized_data) > self.max_buffer_size:
             raise OverflowError(
-                f"se esperaba 'data' <= {self.buffer_size} bytes, se tiene "
-                f"'data' == {len(serialized_data)} bytes."
+                f"el tamaño del mensaje es superior a {self.max_buffer_size} bytes"
             )
-        self._connection.sendall(serialized_data)
-        self._connection.shutdown(socket.SHUT_RD)
-        self._connection.close()
+        amount_sent = self._conn.send(serialized_data)
+
+        self._conn.shutdown(socket.SHUT_RD)
+        self._conn.close()
 
     def close(self) -> None:
         """Cierra el socket."""
-        self.unix_socket.shutdown(socket.SHUT_RD)
-        self.unix_socket.close()
-        os.unlink(self.socket_path)
-
-    def get_data_size(self, data: dict[str, str]) -> int:
-        """Devuelve el tamaño de los datos serializados que se enviarán."""
-        return len(self._serialize(data))
-
-    def _mksocket(self) -> socket.socket:
-        """Crea un nuevo socket UNIX."""
-        return socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-    def _serialize(self, data: dict[str, str]) -> bytes:
-        json_data = json.dumps(data, separators=(",", ":"))
-        return json_data.encode("utf-8")
-
-    def _deserialize(self, data: bytes) -> dict[str, str]:
-        json_data = json.loads(data)
-        return json_data
-
-    def _raise_type_error(self, target_type: IPCType) -> None:
-        """Eleva la excepción NotImplementedError para el tipo de IPC."""
-
-        if target_type == IPCType.SERVER and self.type != IPCType.SERVER:
-            raise NotImplementedError("Solo disponible en IPC tipo SERVER")
-        elif target_type == IPCType.CLIENT and self.type != IPCType.CLIENT:
-            raise NotImplementedError("Solo disponible en IPC tipo CLIENT")
+        self._socket.shutdown(socket.SHUT_RD)
+        self._socket.close()
+        os.unlink(self.ipc_socket)
