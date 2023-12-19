1c363063a4d2e11c912be0af153222380cf4046f
Michel Novus <michelnovus@gmail.com>
2023-12-19 00:20:12 -0300

    Comprobación de disponibilidad de paquetes.

--- localdoc/client/client.py
+++ localdoc/client/client.py
@@ -6,6 +6,7 @@ import sys
 from configuration import Config
 from .console import console
 from .arguments import OPT, CMD, arguments, print_help
+from packages import get_available_packages, get_served_packages
 from .execution_tasks import *
 
 
@@ -16,6 +17,8 @@ def run_client(cfg: Config) -> None:
         print_help()
         sys.exit(1)
 
+    available_packages = get_available_packages(cfg.package_dir)
+    served_packages = get_served_packages(cfg.socket_filepath)
     match args["CMD"]:
         case CMD.LIST:
             show_listing_wall(cfg.socket_filepath, cfg.package_dir)
@@ -31,16 +34,26 @@ def run_client(cfg: Config) -> None:
             else:
                 open_in_browser = False
             for package in args["ARG"].values():
-                serve_package(cfg.socket_filepath, package)
+                if package in available_packages:
+                    serve_package(cfg.socket_filepath, package)
+                else:
+                    console.print(
+                        f"El paquete [b]{package}[/b] no está disponible."
+                    )
         case CMD.CLOSE:
             if len(args["ARG"]) == 0:
                 console.print(
-                    "Faltan argumentos! Vea el comando [b]list[/] para "
+                    "Faltan argumentos! Vea el comando [b]list[/b] para "
                     "obtener los argumentos válidos."
                 )
                 sys.exit(1)
             for package in args["ARG"]:
-                close_package(cfg.socket_filepath, package)
+                if package in served_packages:
+                    close_package(cfg.socket_filepath, package)
+                else:
+                    console.print(
+                        f"El paquete [b]{package}[/b] no esta actualmente servido."
+                    )
         case CMD.INSTALL:
             if len(args["ARG"]) == 0:
                 console.print(
@@ -58,7 +71,12 @@ def run_client(cfg: Config) -> None:
                 )
                 sys.exit(1)
             for package in args["ARG"]:
-                uninstall_package(package)
+                if package in available_packages:
+                    uninstall_package(package)
+                else:
+                    console.print(
+                        f"Nombre de paquete [b]{package}[/b] inválido."
+                    )
         case CMD.CLOSE_DAEMON:
             close_daemon(cfg.socket_filepath)
         case _:
