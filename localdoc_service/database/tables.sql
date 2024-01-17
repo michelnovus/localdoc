a2bef5c35fc780abf580f5952d32666628dc240f
Michel Novus <michelnovus@gmail.com>
2024-01-16 21:53:29 -0300

    Arreglado errores de sintaxis de tables.sql

--- localdoc_service/database/tables.sql
+++ localdoc_service/database/tables.sql
@@ -6,21 +6,21 @@
     The "documents" table defines the identification data (name and version) 
     and informational data (insertion date) of documentation files.
 */
-CREATE TABLE IF NOT EXIST documents (
+CREATE TABLE IF NOT EXISTS documents (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     version TEXT NOT NULL,  -- Semver format: "x.y.z"
     insertion_date TEXT NOT NULL,  -- ISO8601 format: "YYYY-MM-DD HH:MM:SS.SSS"
-    id_file TEXT REFERENCES files (hashsum) ON DELETE CASCADE,
+    id_file TEXT REFERENCES files (hashsum) ON DELETE CASCADE
 );
 
 /*
     The "files" table contains the files as binaries and data associated 
     (filesize and type) with them.
 */
-CREATE TABLE IF NOT EXIST files (
+CREATE TABLE IF NOT EXISTS files (
     hashsum TEXT PRIMARY KEY,  -- BLAKE3 hashsum of file BLOB
     file BLOB NOT NULL,
     type TEXT NOT NULL,  -- type of file: tarball, pdf, executable, etc 
-    size INTEGER NOT NULL,  -- size of file in bytes
+    size INTEGER NOT NULL  -- size of file in bytes
 );
