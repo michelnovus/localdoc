c052cccccb664d67f10c63e80dfff0801ec16fe6
Michel Novus <michelnovus@gmail.com>
2024-02-01 18:22:36 -0300

    Función para resolver donde se localiza el unix socket.

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+// [MIT License] Copyright (c) 2024 Michel Novus
+
+use std::env::var;
+use users::{get_current_uid, get_user_by_uid};
+
+/// Resuelve la localización del socket del proceso en el sistema de archivos.
+///
+/// La función busca una variable de entorno llamada `LOCALDOC_SOCKET` y
+/// comprueba si no está vacía. Si tiene contenido la función retorna
+/// un `Option::Some(String)` donde `String` es el contenido de la variable
+/// de entrono.
+///
+/// Si `LOCALDOC_SOCKET` está vacía (no existe o es una cadena de
+/// longitud cero), se intenta obtener el `UID` del usuario activo y
+/// generar la ruta `/run/user/$UID/localdoc/localdoc.socket` envolviendola
+/// en un `Option::Some()`.
+///
+/// Finalmente si no se pudieron obtener ninguna de las anteriores variables
+/// de entrono la función devuelve `Option::None`.
+pub fn resolve_socket_filepath() -> Option<String> {
+    let socket_filepath = var("LOCALDOC_SOCKET").unwrap_or_default();
+    if socket_filepath.is_empty() {
+        return match get_user_by_uid(get_current_uid()) {
+            Some(data) => {
+                let uid = data.uid();
+                Some(String::from(format!(
+                    "/run/user/{uid}/localdoc/localdoc.socket"
+                )))
+            }
+            None => None,
+        };
+    } else {
+        Some(socket_filepath)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// [MIT License] Copyright (c) 2024 Michel Novus
+
+extern crate localdoc_service;
+use localdoc_service::resolve_socket_filepath;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let socket_file = resolve_socket_filepath().unwrap_or_else(|| process::exit(1));
+    println!("{socket_file}");
 }
