65c79895cd6882fe8d17a05072c2899a0e48ae64
Michel Novus <michelnovus@gmail.com>
2024-02-02 18:30:50 -0300

    Estructura y funciones para crear y destruir el directorio raíz del proceso en ejecución.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,95 @@
 // [MIT License] Copyright (c) 2024 Michel Novus
 
 use std::env::var;
+use std::fs;
+use std::io;
+use std::io::{Error, ErrorKind};
+use std::path::PathBuf;
 use users::{get_current_uid, get_user_by_uid};
 
-/// Resuelve la localización del socket del proceso en el sistema de archivos.
+/// Resuelve la localización del directorio raíz del proceso en el sistema de archivos.
 ///
-/// La función busca una variable de entorno llamada `LOCALDOC_SOCKET` y
+/// La función busca una variable de entorno definida en `optional_envar` y
 /// comprueba si no está vacía. Si tiene contenido la función retorna
 /// un `Option::Some(String)` donde `String` es el contenido de la variable
 /// de entrono.
 ///
-/// Si `LOCALDOC_SOCKET` está vacía (no existe o es una cadena de
+/// Si `optional_envar` está vacía (no existe o es una cadena de
 /// longitud cero), se intenta obtener el `UID` del usuario activo y
-/// generar la ruta `/run/user/$UID/localdoc/localdoc.socket` envolviendola
-/// en un `Option::Some()`.
+/// generar la ruta `/run/user/$UID/localdoc` envolviendola con `Option::Some()`.
 ///
 /// Finalmente si no se pudieron obtener ninguna de las anteriores variables
 /// de entrono la función devuelve `Option::None`.
-pub fn resolve_socket_filepath(optional_envar: &str) -> Option<String> {
-    let socket_filepath = var(optional_envar).unwrap_or_default();
-    if socket_filepath.is_empty() {
+pub fn resolve_root_directory(optional_envar: &str) -> Option<String> {
+    let root_directory = var(optional_envar).unwrap_or_default();
+    if root_directory.is_empty() {
         return match get_user_by_uid(get_current_uid()) {
             Some(data) => {
                 let uid = data.uid();
-                Some(String::from(format!(
-                    "/run/user/{uid}/localdoc/localdoc.socket"
-                )))
+                Some(String::from(format!("/run/user/{uid}/localdoc")))
             }
             None => None,
         };
     } else {
-        Some(socket_filepath)
+        Some(root_directory)
+    }
+}
+
+/// Representa el directorio en tiempo de ejecución del proceso.
+pub struct RuntimeDir {
+    root_directory: PathBuf,
+    socket_path: PathBuf,
+}
+
+impl RuntimeDir {
+    pub fn new(dirpath: &str, socket_name: &str) -> RuntimeDir {
+        RuntimeDir {
+            root_directory: PathBuf::from(dirpath),
+            socket_path: PathBuf::from(socket_name),
+        }
+    }
+
+    /// Crea el directorio raiz de los datos del proceso en tiempo de ejecución.
+    pub fn create_root(&self) -> io::Result<()> {
+        if self.root_directory.is_absolute() && !self.root_directory.exists() {
+            fs::create_dir_all(&self.root_directory)
+        } else {
+            io::Result::Err(Error::new(
+                ErrorKind::AlreadyExists,
+                format!("{:?} ya existe.", &self.root_directory),
+            ))
+        }
+    }
+
+    /// Devuelve la ruta absoluta al socket del proceso.
+    pub fn get_socket(&self) -> &PathBuf {
+        &self.socket_path
+    }
+
+    /// Cambia el nombre actual del socket.
+    pub fn set_socket_name(&mut self, name: &str) {
+        self.socket_path.clear();
+        self.socket_path.push(&self.root_directory);
+        self.socket_path.push(name);
+    }
+
+    /// Devuelve la ruta absoluta al directorio raíz del proceso.
+    pub fn get_root(&self) -> &PathBuf {
+        &self.root_directory
+    }
+}
+
+impl Drop for RuntimeDir {
+    fn drop(&mut self) {
+        fs::remove_file(&self.socket_path).unwrap_or_else(|err| {
+            eprintln!("DROP ERROR [{:?}]: {:?}", err.kind(), &self.socket_path,)
+        });
+        fs::remove_dir(&self.root_directory).unwrap_or_else(|err| {
+            eprintln!(
+                "DROP ERROR [{:?}]: {:?}",
+                err.kind(),
+                &self.root_directory,
+            )
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 // [MIT License] Copyright (c) 2024 Michel Novus
 
 extern crate localdoc_service;
-use localdoc_service::resolve_socket_filepath;
+use localdoc_service::{resolve_root_directory, RuntimeDir};
+use std::env::var;
+use std::io::ErrorKind;
 use std::process;
 
 fn main() {
-    let socket_file =
-        resolve_socket_filepath("LOCALDOC_SOCKET").unwrap_or_else(|| process::exit(1));
-    println!("{socket_file}");
+    let runtime_directory = {
+        let root_directory = resolve_root_directory("LOCALDOC_ROOT")
+            .unwrap_or_else(|| process::exit(1));
+        let socket_name = var("LOCALDOC_SOCKET")
+            .unwrap_or_else(|_| String::from("localdoc.socket"));
+        RuntimeDir::new(&root_directory, &socket_name)
+    };
+    match runtime_directory.create_root() {
+        Ok(_) => println!(
+            "Se creó el directorio: {:?}",
+            runtime_directory.get_root()
+        ),
+        Err(err) if err.kind() == ErrorKind::PermissionDenied => {
+            eprintln!(
+                "No tienes permisos para crear: {:?}",
+                runtime_directory.get_root()
+            );
+            process::exit(1);
+        }
+        Err(err) if err.kind() == ErrorKind::AlreadyExists => {
+            eprintln!(
+                "El directorio: {:?} ya existe",
+                runtime_directory.get_root(),
+            );
+            process::exit(1); // Salir por seguridad de los datos en disco
+        }
+        Err(_) => panic!("Error indeterminado"),
+    };
 }
