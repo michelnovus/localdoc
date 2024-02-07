ab27835e6cb4109995ed4a036a34f4acff1f6728
Michel Novus <michelnovus@gmail.com>
2024-02-07 18:40:09 -0300

    Test de integración para comprobar funcionamiento de los comandos.

--- tests/socket_communication.rs
+++ tests/socket_communication.rs
@@ -2,10 +2,35 @@
 
 extern crate localdoc_service;
 
-use localdoc_service::service::Service;
-use std::{os::unix::net::UnixStream, thread};
+use localdoc_service::service::{process::RuntimeDir, Service};
+use std::{
+    io::prelude::*, net::Shutdown, os::unix::net::UnixStream, thread, time,
+};
 
-const SOCKET: &'static str = "/tmp/socket_communication_test";
+const ROOT_DIR: &'static str = "/tmp/socket_communication_test";
+const SOCK_NAME: &'static str = "socket";
+const SOCKET: &'static str = "/tmp/socket_communication_test/socket";
 
 #[test]
-fn socket_communication_test() {}
+fn socket_communication_test() {
+    let runtime_dir = RuntimeDir::new(&ROOT_DIR, &SOCK_NAME);
+    runtime_dir
+        .create_root()
+        .expect("Error al crear los directorios.");
+    let mut service = Service::new(runtime_dir);
+    thread::spawn(move || match service.start() {
+        Ok(()) => println!("Servicio terminado!"),
+        Err(_) => println!("Algo salió mal..."),
+    });
+    thread::sleep(time::Duration::from_secs_f64(0.5));
+
+    {
+        let mut stream = UnixStream::connect(&SOCKET).unwrap();
+        stream.write_all(r#""EXIT""#.as_bytes()).unwrap();
+        stream.shutdown(Shutdown::Write).unwrap();
+        let mut buffer = String::new();
+        stream.read_to_string(&mut buffer).unwrap();
+        stream.shutdown(Shutdown::Read).unwrap();
+        println!("{}", buffer);
+    }
+}
