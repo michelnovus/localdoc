9a92e2bac11107e83c393d8841fff3f87a5ef79f
Michel Novus <michelnovus@gmail.com>
2024-02-12 19:21:40 -0300

    Definido punto de entrada de comandos de lodoctl.

---
+++ lodoctl/src/lib.rs
@@ -0,0 +1,32 @@
+// [MIT License] Copyright (c) 2024 Michel Novus
+
+/// Módulo de funciones que imprimen mensajes en la terminal.
+pub mod print {
+    use std::path::PathBuf;
+
+    /// Imprime una línea con el nombre del programa y su versión.
+    pub fn version(app_path: &PathBuf) {
+        println!(
+            "{} {}",
+            app_path.file_name().unwrap().to_string_lossy(),
+            env!("CARGO_PKG_VERSION")
+        );
+    }
+
+    /// Imprime el típico mensaje de ayuda.
+    pub fn help(app_path: &PathBuf) {
+        println!(
+            "uso: {} {{comando}} [argumento, ...]",
+            app_path.file_name().unwrap().to_string_lossy()
+        );
+        println!();
+        println!("Comandos:");
+        println!("    --help            Imprime esta ayuda");
+        println!("    --version         Muestra la versión actual");
+    }
+
+    /// Imprime una línea que avisa de un comando inválido.
+    pub fn unknown_cmd(cmd: &String) {
+        println!("No se reconoce el comando: `{cmd}`");
+    }
+}
--- lodoctl/src/main.rs
+++ lodoctl/src/main.rs
@@ -1,6 +1,22 @@
 // [MIT License] Copyright (c) 2024 Michel Novus
 
 extern crate lodolib;
-use lodolib::{api, socket};
+use lodoctl::print;
+// use lodolib::{api, process::RuntimeDir, socket};
+use std::env;
+use std::path::PathBuf;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let app_path = PathBuf::from(&args[0]);
+    match args.get(1) {
+        Some(value_version) if value_version == &"--version".to_string() => {
+            print::version(&app_path)
+        }
+        Some(value_unknown) => {
+            print::unknown_cmd(value_unknown);
+            print::help(&app_path);
+        }
+        None => print::help(&app_path),
+    };
+}
